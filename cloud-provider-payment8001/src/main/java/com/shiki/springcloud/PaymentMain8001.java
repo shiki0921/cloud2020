dfa6a7e0daabdac31c672a9b4477c4585eb994e9
shiki <1332441997@qq.com>
2020-05-04 10:17:07 +0800

    初始导入

---
+++ cloud-provider-payment8001/src/main/java/com/shiki/springcloud/PaymentMain8001.java
@@ -0,0 +1,20 @@
+package com.shiki.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shiki
+ * @title:
+ * @projectName cloud2020
+ * @description: TODO
+ * @date 2020/5/3 19:28
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
